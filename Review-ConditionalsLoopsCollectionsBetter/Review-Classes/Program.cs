01421ec4b5001bd63a97b1be8d2a0899545c1685
Clayton Boggs <cboggs2015@hotmail.com>
2019-01-30 11:13:28 -0600

    Added review for Classes

---
+++ Review-ConditionalsLoopsCollectionsBetter/Review-Classes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student clayton = new Student(3, "Clayton", "Boggs", 80000.5);
+
+            Console.ReadKey();
+        }
+    }
+}
